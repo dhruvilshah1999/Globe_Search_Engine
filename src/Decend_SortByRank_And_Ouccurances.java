ef0015ac8049ca7dc00710bc015038d3a4e2d332
dhruvilshah1999 <68918132+dhruvilshah1999@users.noreply.github.com>
2022-08-07 22:31:53 -0400

    Update Decend_SortByRank_And_Ouccurances.java
    CHanges in Sorting

--- src/Decend_SortByRank_And_Ouccurances.java
+++ src/Decend_SortByRank_And_Ouccurances.java
@@ -18,20 +18,6 @@ public class Decend_SortByRank_And_Ouccurances {
 		}   
 		return sortedHashMap;  
 	}
-	//sorts values of given HashMap in ascending order
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static HashMap<String,Integer> sortValues(HashMap<String,Integer> map){		
-		List list = new LinkedList(map.entrySet());  
-		//Custom Comparator  
-		Collections.sort(list, new Comparator(){public int compare(Object o1, Object o2){return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());}});  
-		//copying the sorted list in HashMap to preserve the iteration order  
-		HashMap<String,Integer> sortedHashMap = new LinkedHashMap<String,Integer>();  
-		for (Iterator it = list.iterator(); it.hasNext();){  
-			 Map.Entry<String,Integer> entry = (Map.Entry<String,Integer>) it.next();  
-			sortedHashMap.put(entry.getKey(), entry.getValue());  
-		}   
-		return sortedHashMap;  
-	}
 	
 	//Sorts search output in Descending Rank. ArrayList contains list of files that have the given phrase.
 	public static Map<String,Integer> sortByRank(ArrayList<String> as, String phrase) {
